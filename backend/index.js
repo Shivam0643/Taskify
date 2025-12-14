527adb8db5885067d3857e0745e6c59bc770abfa
Shivam0643 <shivamlashkari246@gmail.com>
2025-12-14 13:56:32 +0530

    update production API url

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,33 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import authRoutes from './routes/authRoutes.js'
-import taskRoutes from './routes/taskRoutes.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
 // Middleware
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
-    credentials: true
-}));
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 //Connection
 connectDB();
 
 //routes
-app.use("/api/user", authRoutes)
-app.use("/api/tasks", taskRoutes)
+app.use("/api/user", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
-app.get("/",(req, res) =>{
-    res.send("Backend is running")
-})
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
