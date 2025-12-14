f8d6d2d0cf545ea4f3cd2b7007fffd084f037df0
Shivam0643 <shivamlashkari246@gmail.com>
2025-12-14 14:06:16 +0530

    url updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskify-n8lw.onrender.com/api",
+  baseURL: "https://taskify-n8lw.onrender.com/",
 });
 
 api.interceptors.request.use((req) => {
